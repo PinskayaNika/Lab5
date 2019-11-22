707890e508d133cc5de19d270cf6a10f739a71c8
nika <pinskaya.nika@mail.ru>
2019-11-23 00:10:08 +0300

    Scripted auto-commit on change (2019-11-23 00:10:08) by gitwatch.sh

--- lab5/src/main/java/com/examples/sresstesting/FindingResult.java
+++ lab5/src/main/java/com/examples/sresstesting/FindingResult.java
@@ -1,7 +1,26 @@
 package com.examples.sresstesting;
 
+//создаем в actorSystem — актор который принимает две команды —
+// поиск уже готового результата тестирования и результат тестрования.
+
 import javafx.util.Pair;
 
 public class FindingResult {
-    Pair
+    Pair<String, Integer> msgPair;
+
+    public FindingResult(Pair<String, Integer> pair) {
+        this.msgPair = pair;
+    }
+
+    public Pair<String, Integer> getMsgPair() {
+        return msgPair;
+    }
+
+    public String getURL() {
+        return msgPair.getKey();
+    }
+
+    public int getCount() {
+        return msgPair.getValue();
+    }
 }
