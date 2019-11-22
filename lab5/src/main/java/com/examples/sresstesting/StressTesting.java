1dfd6bb178f69d7b4acec9395f346946f607b275
nika <pinskaya.nika@mail.ru>
2019-11-22 23:29:09 +0300

    Scripted auto-commit on change (2019-11-22 23:29:09) by gitwatch.sh

--- lab5/src/main/java/com/examples/sresstesting/StressTesting.java
+++ lab5/src/main/java/com/examples/sresstesting/StressTesting.java
@@ -1,5 +1,6 @@
 package com.examples.sresstesting;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
@@ -9,6 +10,15 @@ import java.io.IOException;
 
 public class StressTesting {
     public static void main(String[] args) throws IOException {
+
+        static ActorRef mainActor;
+        private static final String LOCALHOST = "localhost";
+        private static final String SERVER_INFO = "Server online at http://localhost:8080/\nPress RETURN to stop...";
+        private static final String PACKAGE_ID = "packageId";
+        private static final String POST_MESSAGE = "Message was posted";
+        private static final int SERVER_PORT = 8080;
+        private static final int TIMEOUT_MILLIS = 5000;
+
         //Инициализация http сервера в akka
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
