1e82c889849813f6b8c6d5ab5b4b6077aa8f5f59
nika <pinskaya.nika@mail.ru>
2019-11-22 23:45:38 +0300

    Scripted auto-commit on change (2019-11-22 23:45:38) by gitwatch.sh

--- lab5/src/main/java/com/examples/sresstesting/CacheActor.java
+++ lab5/src/main/java/com/examples/sresstesting/CacheActor.java
@@ -1,5 +1,36 @@
 package com.examples.sresstesting;
 
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CacheActor {
-    
+    public class StorageResultsActor extends AbstractActor {
+        private HashMap<Integer, ArrayList<StoreMessage>> store = new HashMap<>();
+
+        @Override
+        public Receive createReceive() {
+            return ReceiveBuilder.create()
+                    .match(MessageProcessingActor.class, req ->
+                            getSender().tell(
+                                    store.get(req.getPackageId()).toArray(),
+                                    ActorRef.noSender()
+                            )
+                    )
+                    .match(StoreCommand.class, msg -> {
+                                if (store.containsKey(msg.getPackageId())) {
+                                    ArrayList<StoreMessage> tests = store.get(msg.getPackageId());
+                                    tests.add(msg.getStorageMessage());
+                                    store.put(msg.getPackageId(), tests);
+                                } else {
+                                    ArrayList<StoreMessage> tests = new ArrayList<>();
+                                    tests.add(msg.getStorageMessage());
+                                    store.put(msg.getPackageId(), tests);
+                                }
+                            }
+                    ).build();
+        }
 }
