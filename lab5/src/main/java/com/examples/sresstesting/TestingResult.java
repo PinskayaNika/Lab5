83f254a666fe68c52ff922eec87ef2ea81f199a1
nika <pinskaya.nika@mail.ru>
2019-11-23 00:11:13 +0300

    Scripted auto-commit on change (2019-11-23 00:11:13) by gitwatch.sh

--- lab5/src/main/java/com/examples/sresstesting/TestingResult.java
+++ lab5/src/main/java/com/examples/sresstesting/TestingResult.java
@@ -1,4 +1,23 @@
 package com.examples.sresstesting;
 
+import javafx.util.Pair;
+
 public class TestingResult {
+    Pair<String, Integer> msgPair;
+
+    public TestingResult(Pair<Integer, Integer> pair) {
+        this.msgPair = pair;
+    }
+
+    public Pair<String, Integer> getMsgPair() {
+        return msgPair;
+    }
+
+    public String getURL() {
+        return msgPair.getKey();
+    }
+
+    public int getCount() {
+        return msgPair.getValue();
+    }
 }
