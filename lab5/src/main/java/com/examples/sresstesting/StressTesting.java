b4f1da8a39a7e916efd2ca329e89f1bf1eee0cb6
nika <pinskaya.nika@mail.ru>
2019-11-29 21:17:02 +0300

    Scripted auto-commit on change (2019-11-29 21:17:02) by gitwatch.sh

--- lab5/src/main/java/com/examples/sresstesting/StressTesting.java
+++ lab5/src/main/java/com/examples/sresstesting/StressTesting.java
@@ -142,10 +142,10 @@ public class StressTesting {
 //                        Source.from(Collections.singletonList(r))
 //                                .toMat(testSink, Keep.right()).run(materializer);
 
-//                                        RunnableGraph<CompletionStage<HttpResponse>> runnableGraph =
-//                                                source.via(testSink).toMat(Sink.last(), Keep.right());
-//                                        CompletionStage<HttpResponse> result = runnableGraph.run(materializer);
-                                        CompletionStage<HttpResponse> result = source.via(testSink).toMat(Sink.last(), Keep.right()).run(materializer);
+                                        RunnableGraph<CompletionStage<HttpResponse>> runnableGraph =
+                                                source.via(testSink).toMat(Sink.last(), Keep.right());
+                                        CompletionStage<HttpResponse> result = runnableGraph.run(materializer);
+                                        //CompletionStage<HttpResponse> result = source.via(testSink).toMat(Sink.last(), Keep.right()).run(materializer);
                                         return result.toCompletableFuture().get();
                                     } catch (NumberFormatException e) {
                                         e.printStackTrace();
