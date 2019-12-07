3a34d083270336372efb40ec1d32d2bec2b84b79
nika <pinskaya.nika@mail.ru>
2019-12-07 10:46:41 +0300

    Scripted auto-commit on change (2019-12-07 10:46:41) by gitwatch.sh

--- lab5/src/main/java/com/examples/sresstesting/StressTesting.java
+++ lab5/src/main/java/com/examples/sresstesting/StressTesting.java
@@ -167,6 +167,7 @@ public class StressTesting {
                                                                                         new javafx.util.Pair<>(data.first(), new javafx.util.Pair<>(data.second(), sum))),
                                                                                         TIMEOUT_MILLIS);
                                                                         Double middleValue = (double) sum / (double) countInteger;
+                                                                        //
                                                                         return CompletableFuture
                                                                                 .completedFuture(HttpResponse
                                                                                         .create().withEntity(ByteString.fromString(FINAL_ANSWER + middleValue.toString())));
